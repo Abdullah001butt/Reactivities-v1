2e12dfcd3c46eca20bbddc2ff34027aaf69ce879
Abdullah <anasbutt2014@gmail.com>
2025-10-20 17:14:44 +0400

    error resolving testing

--- client/src/lib/hooks/useComment.ts
+++ client/src/lib/hooks/useComment.ts
@@ -1,73 +1,61 @@
-import { useLocalObservable } from "mobx-react-lite";
-import {
-  HubConnection,
-  HubConnectionBuilder,
-  HubConnectionState,
-} from "@microsoft/signalr";
+import { useLocalObservable } from "mobx-react-lite"
+import {HubConnection, HubConnectionBuilder, HubConnectionState} from '@microsoft/signalr';
 import { useEffect, useRef } from "react";
 import { runInAction } from "mobx";
 
-export interface ChatComment {
-  id: string;
-  body: string;
-  username: string;
-  createdAt: string;
-}
-
 export const useComments = (activityId?: string) => {
-  const created = useRef(false);
+    const created = useRef(false);
+    const commentStore = useLocalObservable(() => ({
+        comments: [] as ChatComment[],
+        hubConnection: null as HubConnection | null,
 
-  const commentStore = useLocalObservable(() => ({
-    comments: [] as ChatComment[],
-    hubConnection: null as HubConnection | null,
+        createHubConnection(activityId: string) {
+            if (!activityId) return;
 
-    createHubConnection(activityId?: string) {
-      if (!activityId) return;
+            this.hubConnection = new HubConnectionBuilder()
+                .withUrl(`${import.meta.env.VITE_COMMENTS_URL}?activityId=${activityId}`, {
+                    withCredentials: true
+                })
+                .withAutomaticReconnect()
+                .build();
 
-      this.hubConnection = new HubConnectionBuilder()
-        .withUrl(`${import.meta.env.VITE_COMMENTS_URL}?activityId=${activityId}`, {
-          withCredentials: true,
-        })
-        .withAutomaticReconnect()
-        .build();
+            this.hubConnection.start().catch(error => 
+                console.log('Error establishing connection: ', error));
 
-      this.hubConnection
-        .start()
-        .catch((error: unknown) => console.error("Error establishing connection:", error));
+            this.hubConnection.on('LoadComments', comments => {
+                runInAction(() => {
+                    this.comments = comments
+                })
+            });
 
-      this.hubConnection.on("LoadComments", (comments: ChatComment[]) => {
-        runInAction(() => {
-          this.comments = comments;
-        });
-      });
+            this.hubConnection.on('ReceiveComment', comment => {
+                runInAction(() => {
+                    this.comments.unshift(comment);
+                })
+            })
+        },
 
-      this.hubConnection.on("ReceiveComment", (comment: ChatComment) => {
-        runInAction(() => {
-          this.comments.unshift(comment);
-        });
-      });
-    },
+        stopHubConnection() {
+            if (this.hubConnection?.state === HubConnectionState.Connected) {
+                this.hubConnection.stop().catch(error => 
+                    console.log('Error stopping connection: ', error))
+            }
+        }
+    }));
 
-    stopHubConnection() {
-      if (this.hubConnection?.state === HubConnectionState.Connected) {
-        this.hubConnection
-          .stop()
-          .catch((error: unknown) => console.error("Error stopping the connection:", error));
-      }
-    },
-  }));
+    useEffect(() => {
+        if (activityId && !created.current) {
+            commentStore.createHubConnection(activityId);
+            created.current = true
+        }
 
-  useEffect(() => {
-    if (activityId && !created.current) {
-      commentStore.createHubConnection(activityId);
-      created.current = true;
+        return () => {
+            commentStore.stopHubConnection();
+            commentStore.comments = [];
+        }
+    }, [activityId, commentStore])
+
+    return {
+        commentStore
     }
-
-    return () => {
-      commentStore.stopHubConnection();
-      commentStore.comments = [];
-    };
-  }, [activityId, commentStore]);
-
-  return { commentStore };
-};
+}
